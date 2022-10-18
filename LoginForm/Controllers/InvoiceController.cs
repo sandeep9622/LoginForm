a49f43231ed0985539d63dbf01660521a6052a6d
sandeep9622 <sandeep.sappal96@gmail.com>
2022-10-19 00:15:10 +0530

    Added invoice controller and model

---
+++ LoginForm/Controllers/InvoiceController.cs
@@ -0,0 +1,21 @@
+﻿using LoginForm.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LoginForm.Controllers
+{
+    [Route("Invoice")]
+    public class InvoiceController:Controller
+    {
+        [Route("Save")]
+        [HttpPost]
+        public IActionResult SaveInvoice(InvoiceModel invoice)
+        {
+
+            return View();
+        }
+    }
+}
---
+++ LoginForm/Models/InvoiceModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LoginForm.Models
+{
+    public class InvoiceModel
+    {
+        public int GRNo { get; set; }
+        public int Date { get; set; }
+        public int vehicalNo { get; set; }
+        public int From { get; set; }
+        public int To { get; set; }
+        public int ValueOfGoods { get; set; }
+        public int InvoiceNo { get; set; }
+        public int ConsignerName { get; set; }
+        public int ConsignerGSTNo { get; set; }
+        public int ConsignerMobile { get; set; }
+        public int ConsignerEmail { get; set; }
+        public int ConsignerAddress { get; set; }
+        public int ConsigneeName { get; set; }
+        public int ConsigneeGSTNo { get; set; }
+        public int ConsigneeMobile { get; set; }
+        public int ConsigneeEmail { get; set; }
+        public int ConsigneeAddress { get; set; }
+    }
+}
